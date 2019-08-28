f6452677ff510bcfe78067060f1168d4701eeafe
code.qian <qian.wolf@gmail.com>
2019-08-29 00:46:41 +0800

    bak

---
+++ app/src/main/java/net/codepig/mvpdemo/MyApp.java
@@ -0,0 +1,17 @@
+package net.codepig.mvpdemo;
+
+import android.app.Application;
+
+public class MyApp extends Application {
+    private static MyApp instance;
+
+    public static MyApp getInstance() {
+        return instance;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        instance = this;
+    }
+}
