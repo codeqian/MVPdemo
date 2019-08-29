84222abc86d8ca9be116f3b75a1359fccd5da1cb
code.qian <qian.wolf@gmail.com>
2019-08-29 22:36:36 +0800

    添加glide库

--- mvp/src/main/java/net/codepig/mvp/util/ImageUtil.java
+++ mvp/src/main/java/net/codepig/mvp/util/ImageUtil.java
@@ -1,11 +1,48 @@
 package net.codepig.mvp.util;
 
+import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 /**
  * 封装图片加载
  */
 public class ImageUtil {
-    public static void loadImage(String url, ImageView iv, int placeHolder, int errorHolder) {
+    private static Drawable drawable;
+    private static Drawable errorDrawable;
+    public static void imageConfig(Drawable _drawable) {
+        drawable=_drawable;
+    }
+
+    public static void imageConfig(Drawable _drawable, Drawable _errorDrawable) {
+        drawable=_drawable;
+        errorDrawable=_errorDrawable;
+    }
+
+    public static void loadImageNoDefault(Context context, String url, ImageView iv) {
+        Glide.with(context)
+                .load(url)
+                .into(iv);
+    }
+
+    public static void loadImage(Context context, String url, ImageView iv) {
+        Glide.with(context)
+                .load(url)
+                .placeholder(drawable)
+                .into(iv);
+    }
+
+    public static void loadImage(Context context, String url, ImageView iv, int w,int h) {
+        Glide.with(context)
+                .load(url)
+                .placeholder(drawable)
+                .override(w, h)
+                .into(iv);
+    }
+
+    public static void clearImage(Context context,ImageView iv){
+        Glide.with(context).clear(iv);
     }
 }
